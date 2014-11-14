34fea20f7f14cd4f8ef6aa4b9d219d921eebe479
webb365 <admin@webb365.se>
2014-11-14 20:49:46 +0100

    klar

--- 1-vandalen/vandalen/script.js
+++ 1-vandalen/vandalen/script.js
@@ -1,9 +1,40 @@
 "use strict";
 
 var makePerson = function(persArr){
-
-
-	// Din kod här...
+	var minAge = 150;
+	var maxAge = 0;
+	var avarageAge = 0;
+	var age_arr = new Array();
+	var names_arr = new Array();
+	var names = '';
+	var i = 0;
+	persArr.forEach(function(value) {
+    	console.log(value);
+    	names_arr[i] = value.name;	    	
+		age_arr[i] = value.age;
+    	if(value.age < minAge){
+	    	minAge = value.age;
+    	}
+     	if(value.age > maxAge){
+	    	maxAge = value.age;
+    	}  
+    	i++; 	
+	});
+	names_arr.sort(String.localeCompare)
+	names_arr.forEach(function(value) {
+    	if(names.length>0){
+	    	names += ', ' + value;
+    	}else{
+	    	names = value;
+    	}
+    	
+	});
+	age_arr.forEach(function(value) {
+		avarageAge += value; 
+	});	
+	avarageAge = Math.round(avarageAge / age_arr.length);
+	
+	return {minAge: minAge, maxAge: maxAge, names: names, averageAge: avarageAge};
 
 }
 
