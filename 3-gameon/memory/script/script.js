87676d73b5e1e09fa43308096e31687e853aca02
webb365 <admin@webb365.se>
2014-12-12 02:45:37 +0100

    Andra

--- 3-gameon/memory/script/script.js
+++ 3-gameon/memory/script/script.js
@@ -1,6 +1,6 @@
 "use strict";
 
-var mem = new memory(4,4);
+var mem = new memory(8,4);
 mem.toHTML();
 function memory(_cols,_rows)
 {
@@ -11,19 +11,71 @@ function memory(_cols,_rows)
 
 
 this.toHTML = function() {
-		var html = '';
+		var i = 0;
+		var html = '<div class="row">';
 		var current_col = 0;
+		var current_row = 0;
+		var width = Math.floor(12/cols);
 		rand_array.forEach(function (item){
-		  html += item;
+		  html += '<div class="col-sm-' + width + '"><a onclick="mem.click(\'' + i + '\');"></a></div>';
 		  current_col++;
+		  i++;
 		  if(current_col==cols){
 			  current_col = 0;
-			  html += '#';
+			  current_row++;
+			  if(current_row==rows){
+				  html += '</div>';
+			  }else{
+			 	 html += '</div><div class="row">';
+			  }
 		  }
 		});
-		console.log(html);
+		document.getElementById("memory").innerHTML = html;
         return html;
    };
+  
+  
+  
+  
+   this.getIconHTML = function(id) {
+	   var icon = '';
+	   switch (id) {
+		    case 0:
+		        icon = "fa-question";
+		        break;
+		    case 1:
+		        icon = "fa-btc";
+		        break;
+		    case 2:
+		        icon = "fa-cc-mastercard";
+		        break;
+		    case 3:
+		        icon = "fa-android";
+		        break;
+		    case 4:
+		        icon = "fa-apple";
+		        break;
+		    case 5:
+		        icon = "fa-cc-visa";
+		        break;
+		    case 6:
+		        icon = "fa-github-alt";
+		        break;
+			case 7:
+		        icon = "fa-spotify";
+		        break;
+		    case 8:
+		        icon = "fa-wordpress";
+		        break;
+		    case 9:
+		        icon = "fa-steam";
+		        break;
+		    case 10:
+		        icon = "fa-paypal";
+		        break;
+		} 
+       return icon;
+   }
 
 /*   this.getText = function() {
        return text;
@@ -56,6 +108,11 @@ this.toHTML = function() {
    var __construct = function() {
 		rows =  _rows;
 		cols = _cols;
+		if(cols>12||cols*rows > 20){
+			console.log('#######################################################');
+			console.log('# Spelet kommer ej köras pga felaktiga värden i koden #');
+			console.log('#######################################################');
+		}
 		rand_array = RandomGenerator.getPictureArray(rows,cols);
 		
    }()
