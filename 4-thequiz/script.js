267ceee31683a0819b04c43cc09d73724bbea403
webb365 <admin@webb365.se>
2014-12-15 15:20:17 +0100

    Fixat

--- 4-thequiz/script.js
+++ 4-thequiz/script.js
@@ -2,7 +2,9 @@
 
 var Quiz = {
 	try_count:0,
+	current_count:0,
 	nextUrl: '',
+	array_z: [],
 	init:function(){
 		document.getElementById("answer_btn").addEventListener("click", Quiz.postAnswer);
 		Quiz.proccesQuestion('http://vhost3.lnu.se:20080/question/1');
@@ -31,14 +33,27 @@ var Quiz = {
 				document.getElementById('status').innerHTML  = '';
 				var response = JSON.parse(request.responseText);
 				if(response.nextURL != null){
+					Quiz.current_count++;
+					Quiz.array_z.push(Quiz.current_count);
+					Quiz.current_count = 0;
 					Quiz.proccesQuestion(response.nextURL);
 				}else{
-					document.getElementById('status').innerHTML  = '<div class="alert alert-success" role="alert">Du klarade quizen på ' + Quiz.try_count + ' försök!</div>';
+					Quiz.current_count++;
+					Quiz.array_z.push(Quiz.current_count);
+					var i = 1;
+					var string = '';
+					Quiz.array_z.forEach(function(value) {
+					    string += 'Fråga '+i + ': ' +value + ' försök.<br>';
+						i++;	
+					});
+					document.getElementById('status').innerHTML  = '<div class="alert alert-success" role="alert">Du klarade quizen på ' + Quiz.try_count + ' försök!</div>'+string;
+					console.log(Quiz.array_z);
 					document.getElementById('question').innerHTML  = '';
 					document.getElementById('answer').innerHTML  = '';
 				}
 			  }else if(request.readyState==4 && request.status==400){
 				document.getElementById('status').innerHTML  = '<div class="alert alert-danger" role="alert">Fel svar</div>';
+				Quiz.current_count++;
 			  }
 		  }
 	},
