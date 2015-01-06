d088ca7fcb51b44199a07e6c1a96e00c148ce6b3
webb365 <admin@webb365.se>
2015-01-06 21:16:46 +0100

    Ska utvecklar funktioner för att uppnå steg 3

--- 5-pwr/script.js
+++ 5-pwr/script.js
@@ -13,25 +13,56 @@ var Computer = {
 		});	
 	},
 	start:function(){
-	  var template = $('#computer-template').html();
-	  Mustache.parse(template);
-	  var rendered = Mustache.render(template);
-	  $('.container').html(rendered);
-	  $('#stop').click(function(){
+		var template = $('#computer-template').html();
+		Mustache.parse(template);
+		var rendered = Mustache.render(template);
+		$('.container').html(rendered);
+		$('#stop').click(function(){
 		console.log('Stänger av datorn.');
-		Computer.shutdown();
-	  });	
-	  console.log('Datorn startad.');
+			Computer.shutdown();
+		});
+		Computer.windowmanager.init();	
+		console.log('Datorn startad.');
 	},
 	shutdown:function(){
-	  var template = $('#start-template').html();
-	  Mustache.parse(template);
-	  var rendered = Mustache.render(template);
-	  $('.container').html(rendered);
-	  $('#start').click(function(){
-		Computer.start();
-	  });	
-	  console.log('Datorn avstängd.');
+		var template = $('#start-template').html();
+		Mustache.parse(template);
+		var rendered = Mustache.render(template);
+		$('.container').html(rendered);
+		$('#start').click(function(){
+			Computer.start();
+		});	
+		console.log('Datorn avstängd.');
+	},
+	windowmanager:{
+		widows:[],
+		init:function(){
+		    $('#start-camera').click(function(){
+				console.log('Startar fönster.');
+				Computer.windowmanager.create_window(1);
+		    });
+		},
+		create_window:function(appid){
+			Computer.windowmanager.widows.push(appid);	
+			Computer.windowmanager.render();		
+		},
+		render:function(){
+			var window_id = 1;
+			$('#main').html('');
+			Computer.windowmanager.widows.forEach(function(appid) {
+				if(appid==1){
+					var app_meta = {id:window_id,titel: "Bildvisare",footer: '',body:''};
+				}else{
+					var app_meta = {titel: "FEL",id:'000',footer: 'FEL',body:'Starta om datorn för att fixa felet.'};
+				}
+				var template = $('#window-template').html();
+				Mustache.parse(template);
+				var rendered = Mustache.render(template, app_meta);
+				$('#main').append(rendered);
+				console.log('Fönster ' + window_id + ' renderat.');
+				window_id++;
+			});
+		}
 	}
 };
 
