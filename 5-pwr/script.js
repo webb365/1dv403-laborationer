1f4be08b5e624c5ab8ce051fc6f44246670811bc
webb365 <admin@webb365.se>
2015-01-06 19:46:34 +0100

    Ska börja utveckla funktioner för att uppnå steg 3

--- 5-pwr/script.js
+++ 5-pwr/script.js
@@ -2,15 +2,39 @@
 
 var Computer = {
 	init:function(){
+		console.log('Initsierar platformen.');
+		var template = $('#start-template').html();
+		Mustache.parse(template);
+		var rendered = Mustache.render(template);
+		$('.container').html(rendered);
+		$('#start').click(function(){
+			console.log('Startar datorn.');
+			Computer.start();
+		});	
+	},
+	start:function(){
 	  var template = $('#computer-template').html();
 	  Mustache.parse(template);
-	  var rendered = Mustache.render(template, {name: "Luke"});
+	  var rendered = Mustache.render(template);
 	  $('.container').html(rendered);
+	  $('#stop').click(function(){
+		console.log('Stänger av datorn.');
+		Computer.shutdown();
+	  });	
+	  console.log('Datorn startad.');
+	},
+	shutdown:function(){
+	  var template = $('#start-template').html();
+	  Mustache.parse(template);
+	  var rendered = Mustache.render(template);
+	  $('.container').html(rendered);
+	  $('#start').click(function(){
+		Computer.start();
+	  });	
+	  console.log('Datorn avstängd.');
 	}
 };
 
 $( document ).ready(function() {
-	$('#start').click(function(){
-		Computer.init();
-	});	
+	Computer.init();
 });
