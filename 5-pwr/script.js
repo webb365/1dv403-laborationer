c5c1077ee9b639309bc5fb60f346f60f501dca26
webb365 <admin@webb365.se>
2015-01-07 00:22:48 +0100

    Ska utvecklar funktioner för att uppnå steg 3

--- 5-pwr/script.js
+++ 5-pwr/script.js
@@ -36,6 +36,7 @@ var Computer = {
 	},
 	windowmanager:{
 		widows:[],
+		layer: 1,
 		init:function(){
 		    $('#start-camera').click(function(){
 				console.log('Startar fönster.');
@@ -43,17 +44,26 @@ var Computer = {
 		    });
 		},
 		create_window:function(appid){
-			Computer.windowmanager.widows.push(appid);	
+			Computer.windowmanager.widows.push({id:appid,x:0,y:0});	
 			Computer.windowmanager.render();		
 		},
 		close_window:function(id){
-			Computer.windowmanager.widows.splice(id, 1);
+			Computer.windowmanager.widows.splice((id-1), 1);
 			Computer.windowmanager.render();
+		},
+		move_window:function(e,id){
+			if(e.pageY>33&&e.pageY<($('#desktop').height()-125)&&e.pageX>180&&e.pageX<($('#desktop').width()-68)){
+				$('#window-'+id).css({"top":(e.pageY-35)+'px',"left":(e.pageX-180)+'px'});
+			}
+			Computer.windowmanager.widows[(id-1)].x=e.pageX-180;
+			Computer.windowmanager.widows[(id-1)].y=e.pageY-35;
+			
 		},
 		render:function(){
 			var window_id = 1;
 			$('#main').html('');
-			Computer.windowmanager.widows.forEach(function(appid) {
+			Computer.windowmanager.widows.forEach(function(obj) {
+				var appid = obj.id;
 				if(appid==1){
 					var app_meta = {id:window_id,titel: '<i class="fa fa-camera-retro"></i> Bildvisare',footer: '',body:''};
 				}else{
@@ -63,10 +73,26 @@ var Computer = {
 				Mustache.parse(template);
 				var rendered = Mustache.render(template, app_meta);
 				$('#main').append(rendered);
+				$('#window-'+window_id).css({"top":(Computer.windowmanager.widows[(window_id-1)].y)+'px',"left":(Computer.windowmanager.widows[(window_id-1)].x)+'px'});
 				console.log('Fönster ' + window_id + ' renderat.');
 				$('#quit-'+window_id).click(function(){
-					Computer.windowmanager.close_window(this.id);
+					var id = $(this).attr("data-id");
+					Computer.windowmanager.close_window(id);
 				});	
+				$('.window').on('mousedown',function() {
+					var window_id = $(this).attr("data-id");
+					$('#window-'+window_id).css('z-index',Computer.windowmanager.layer);
+					Computer.windowmanager.layer++;
+				});
+				$('.panel-heading').on('mousedown',function() {
+					var window_id = $(this).attr("data-id");
+					$(window).on('mousemove',function( event ) {
+						Computer.windowmanager.move_window(event,window_id);
+					});
+				});
+				$('.panel-heading').on('mouseup',function() {
+					$(window).off('mousemove');
+				});
 				window_id++;
 			});
 		}
